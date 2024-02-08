e13d49dac0445f8b091389d14fab1dcbc70bc61d
godls2513 <godls2300@gmail.com>
2024-02-08 18:31:40 +0900

    프로젝트 푸쉬 테스트

---
+++ rest/src/main/java/com/chafy/api/rest/ShoppingmallRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.chafy.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingmallRestApiApplication.class, args);
+	}
+
+}
---
+++ rest/src/test/java/com/chafy/api/rest/ShoppingmallRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chafy.api.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
