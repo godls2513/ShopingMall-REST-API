301b05c2c5d2a944d68ed6c8d6d23309a1888a01
godls2513 <godls2300@gmail.com>
2024-02-08 18:52:44 +0900

    project create!!

--- rest/src/main/java/com/chafy/api/rest/ShoppingMallApplication.java
+++ rest/src/main/java/com/chafy/api/rest/ShoppingMallApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppingmallRestApiApplication {
+public class ShoppingMallApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShoppingmallRestApiApplication.class, args);
+		SpringApplication.run(ShoppingMallApplication.class, args);
 	}
 
 }
