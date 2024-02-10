c8611f72cb1a9f6d38e69b85cde525944714b571
godls2513 <godls2300@gmail.com>
2024-02-10 21:21:47 +0900

    update source

--- README.md
+++ README.md
@@ -4,26 +4,27 @@
 ### 로그인/회원가입
 - 로그인 POST /session
 - 로그아웃 DELETE /session
-- 회원가입 POST /join/{user_id}
 
 ### 내 정보
 - 내 정보 조회 GET /users/me
 - 내 정보 수정 PATCH /users/me
+- 회원가입 POST /users
 
 ### 상품
 - 상품 목록 GET /items
 - 상품 상세 GET /items/{item_id}
 
 ### 상품 리뷰
-- 상품 리뷰 작성 POST /itmes/{item_id}/comments
-- 상품 리뷰 수정 PATCH /itmes/{item_id}/comments/{comment_id}
+- 상품 리뷰 작성 POST /items/{item_id}/comments
+- 상품 리뷰 수정 PATCH /items/{item_id}/comments/{comment_id}
 - 상품 리뷰 삭제 DELETE /items/{item_id}/comments/{comment_id}
 
 ### 장바구니
-- 장바구니에 상품 추가 POST /carts/{item_id} 
+- 장바구니에 상품 추가 POST /carts/{item_id}
 - 장바구니에 상품 삭제 DELETE /carts/{item_id}
 - 장바구니 전체 목록 GET /carts
 
 ### 구매
 - 주문하기 POST /orders
 - 주문목록 GET /orders
+
--- shoppingmall/src/main/java/com/chafy/api/rest/shoppingmall/controllers/cartController.java
+++ shoppingmall/src/main/java/com/chafy/api/rest/shoppingmall/controllers/cartController.java
@@ -7,19 +7,23 @@ import org.springframework.web.bind.annotation.*;
 public class cartController {
     // 장바구니 목록
     @GetMapping("/")
-    public String list() {
-        return "장바구니 목록\n";
+    public String list(@RequestAttribute String userId) {
+        return userId + " 의 장바구니 목록\n";
     }
 
     // 장바구니에 상품 추가
     @PostMapping("/{item_id}")
-    public String addItem(@PathVariable("item_id") String id) {
-        return "장바구니에 상품 추가\n";
+    public String addItem(
+            @RequestAttribute String userId,
+            @PathVariable("item_id") String id) {
+        return userId + "의 장바구니에 상품 " + id + "(이)가 추가\n";
     }
 
     // 장바구니 상품 삭제
     @DeleteMapping("/{item_id}")
-    public String deleteItem(@PathVariable("item_id") String id) {
-        return "장바구니에 상품 삭제\n";
+    public String deleteItem(
+            @RequestAttribute String userId,
+            @PathVariable("item_id") String id) {
+        return userId + "의 장바구니에 상품 " + id + "(이)가 삭제\n";
     }
 }
