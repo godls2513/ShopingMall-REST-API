7b5b29004ec9d422c461e61fe9a13e282af9dffa
godls2513 <godls2300@gmail.com>
2024-02-10 10:39:40 +0900

    mod

---
+++ shoppingmall/src/test/java/com/chafy/api/rest/shoppingmall/ShoppingmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chafy.api.rest.shoppingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
